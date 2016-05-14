547356a5836d02f822e3d0c0b4eff1baa0533c73
William Moore <w.moore@dundee.ac.uk>
2016-05-14 23:18:33 +0100

    Split histogram.js up into separate Model and UI components

--- static/webtest/js/histogram.js
+++ static/webtest/js/histogram.js
@@ -1,5 +1,121 @@
 
-$(document).ready(function(){
+
+
+// UI components
+// ------------------------------------------------------
+var Zslider = function(model) {
+    $("#zSlider").on('input', function(){
+        $("#zIndex").html($(this).val());
+    }).on('change', function(){
+        var theZ = $(this).val();
+        model.set('theZ', theZ);
+    });
+};
+
+
+var ImageViewer = function(model) {
+
+    model.on('change:theZ change:theT change:channels', function(model){
+        console.log("Z/T or channels changed");
+
+        var cStrings = [];
+        _.each(model.get('channels'), function(c, i){
+            cStrings.push(1+i + "|" + c.window.start + ":" + c.window.end + "$" + c.color);
+        });
+        var renderString = cStrings.join(","),
+            imageId = model.get('id'),
+            theZ = model.get('theZ'),
+            theT = model.get('theT');
+
+        var imgSrc = '/webgateway/render_image/' + imageId + "/" + theZ + "/" + theT +
+                '/?c=' + renderString + "&m=c";
+
+        console.log('updateImage', imgSrc);
+        $("#viewer").attr('src', imgSrc);
+    });
+};
+
+
+// Backbone model
+// ------------------------------
+var ViewerModel = Backbone.Model.extend({
+
+    defaults: {
+        selectedChannelIdx: 0
+    },
+
+    loadData: function(imgId) {
+        $.getJSON("/webgateway/imgData/" + imgId + "/", function(data){
+            console.log(data);
+            console.log(this);
+
+            data.theT = data.rdefs.defaultT;
+            data.theZ = data.rdefs.defaultZ;
+            this.set(data);
+        }.bind(this));
+    },
+
+    setChannelWindow: function(idx, start, end) {
+        var oldChs = this.get('channels');
+        // Need to clone the list of channels...
+        var chs = [];
+        for (var i=0; i<oldChs.length; i++) {
+            chs.push($.extend(true, {}, oldChs[i]));
+        }
+        // ... then set new value ...
+        chs[idx].window.start = start;
+        chs[idx].window.end = end;
+        // ... so that we get the changed event triggering OK
+        this.set('channels', chs);
+    }
+});
+
+
+var ChannelSliders = function(model) {
+
+        // Build sliders when image loads....
+    model.on('change:id', function(model){
+        console.log("Image loaded: build channels...");
+        console.log(arguments);
+
+        var channels = model.get('channels');
+        buildChannels(channels);
+    });
+
+
+    var buildChannels = function(channels) {
+
+        $('#sliders').empty();
+
+        channels.forEach(function(ch, idx){
+            $("<div style='background: #" + ch.color + "'></div>")
+                .appendTo('#sliders')
+                .slider({
+                    range: true,
+                    min: ch.window.min,
+                    max: ch.window.max,
+                    values: [ch.window.start, ch.window.end],
+                    start: function() {
+                        // plotHistogram(idx);
+                        model.set('selectedChannelIdx', idx);
+                    },
+                    slide: function(event, ui) {
+                        // we don't setChannelWindow() here
+                        // since that would refresh image during slide
+                        model.trigger('slide', ui.values);
+                    },
+                    stop: function(event, ui) {
+                        console.log('stop', ui.values);
+                        model.setChannelWindow(idx, ui.values[0], ui.values[1]);
+                    }
+                });
+        });
+    };
+};
+
+
+var Histogram = function(model) {
+
     var width = 512;
     var height = 512;
     var pixels;
@@ -7,16 +123,12 @@ $(document).ready(function(){
     var ctx;
     var svg;
     var chart;
-    // We cache the histogram data (3 channels) and plot whichever
-    // is the active slider
+
     var hdata = [];
     var lastChIdx = 0;
-
-    $("#canvas").css({'width': width +'px', 'height': height +'px'});
-
+    // img not displayed
     var img = new Image();
 
-    // set up the svg histogram...
     var n = 256,
         margin = {top: 20, right: 0, bottom: 20, left: 0},
         testWidth = 512 - margin.left - margin.right,
@@ -43,64 +155,44 @@ $(document).ready(function(){
     lineEnter.attr("width", 1);
     lineEnter.attr("x", function(d, i) { return d * (testWidth/2); });
 
-
-
-    var ViewerModel = Backbone.Model.extend({
-
-        loadData: function(imgId) {
-            $.getJSON("/webgateway/imgData/" + imgId + "/", function(data){
-                console.log(data);
-                console.log(this);
-
-                data.theT = data.rdefs.defaultT;
-                data.theZ = data.rdefs.defaultZ;
-                this.set(data);
-            }.bind(this));
-        },
-
-        setChannelWindow: function(idx, start, end) {
-            var oldChs = this.get('channels');
-            // Need to clone the list of channels...
-            var chs = [];
-            for (var i=0; i<oldChs.length; i++) {
-                chs.push($.extend(true, {}, oldChs[i]));
-            }
-            // ... then set new value ...
-            chs[idx].window.start = start;
-            chs[idx].window.end = end;
-            // ... so that we get the changed event triggering OK
-            this.set('channels', chs);
-        }
-    });
-
-
-    var model = new ViewerModel();
-
-
-    // Build sliders when image loads....
-    model.on('change:id', function(model){
-        console.log("Image loaded: build channels...");
-        console.log(arguments);
-
-        var channels = model.get('channels');
-        buildChannels(channels);
-    });
-
-
     // load histogram data when new plane chosen
     model.on('change:theZ change:theT', function(model){
         console.log("Z/T changed");
         console.log(arguments);
 
-
         loadHistogramData(model);
     });
+
+    model.on('change:selectedChannelIdx', function(event, idx, model){
+
+        console.log('change:selectedChannelIdx', arguments);
+        plotHistogram(idx);
+    });
+
+    // when 'slide' is triggered (no model change), we 
+    // update slider markers on histogram
+    model.on('slide', function(args){
+
+        var start = args[0],
+            end = args[1],
+            idx = model.get('selectedChannelIdx'),
+            ch = model.get('channels')[idx],
+            min = ch.window.min,
+            max = ch.window.max;
+        start = ((start - min)/(max - min)) * 256;
+        end = ((end - min)/(max - min)) * 256;
+
+        chartRange([start, end], '#' + ch.color);
+    });
+
+    var histogramStart;
 
     var loadHistogramData = function(model) {
 
         console.log('loadHistogramData...');
         // we want 'full range' image (darkest to brightest pixels)
         // for the current Z/T plane
+        histogramStart = new Date();
 
         var cStrings = [];
         _.each(model.get('channels'), function(c, i){
@@ -122,8 +214,8 @@ $(document).ready(function(){
     // When we have the Image data, use it to populate the canvas
     img.onload = function() {
         canvas = document.getElementById("canvas");
-        canvas.width = width;
-        canvas.height = height;
+        // canvas.width = width;
+        // canvas.height = height;
 
         ctx = canvas.getContext("2d");
         ctx.drawImage(img, 0, 0);
@@ -134,46 +226,10 @@ $(document).ready(function(){
         cacheHistogramData(raw);
 
         // plot current channel
-        plotHistogram(lastChIdx);
-    };
+        var selectedChannelIdx = model.get('selectedChannelIdx');
+        plotHistogram(selectedChannelIdx);
 
-
-    var buildChannels = function(channels) {
-
-        $('#sliders').empty();
-
-        channels.forEach(function(ch, idx){
-            $("<div style='background: #" + ch.color + "'></div>")
-                .appendTo('#sliders')
-                .slider({
-                    range: true,
-                    min: ch.window.min,
-                    max: ch.window.max,
-                    values: [ch.window.start, ch.window.end],
-                    start: function() {
-                        plotHistogram(idx);
-                    },
-                    slide: function(event, ui) {
-                        // renderString(idx, ui.values[0], ui.values[1]);
-
-                        // histogram has 256 columns, from min - max
-                        // need to scale start and end within this range
-                        var start = ui.values[0],
-                            end = ui.values[1],
-                            min = ch.window.min,
-                            max = ch.window.max;
-                        start = ((start - min)/(max - min)) * 256;
-                        end = ((end - min)/(max - min)) * 256;
-                        chartRange([start, end], '#' + ch.color);
-                        // update image viewer canvas (not currently working)
-                        // renderString(idx, ui.values[0], ui.values[1]);
-                    },
-                    stop: function(event, ui) {
-                        console.log('stop', ui.values);
-                        model.setChannelWindow(idx, ui.values[0], ui.values[1]);
-                    }
-                });
-        });
+        console.log("Histogram loading took", new Date() - histogramStart);
     };
 
 
@@ -228,65 +284,26 @@ $(document).ready(function(){
             .attr('stroke', color);
     };
 
-
-    // Update the specified channel of the image
-    // var renderString = function(channel, start, end) {
-
-    //     // get the current pixel data...
-    //     pixels = ctx.getImageData(0, 0, width, height);
-
-    //     var d;
-    //     // go through all pixels of the specified channel (r,g,b or a)
-    //     for (var i = channel, n = pixels.data.length; i < n; i+=4) {
-    //         d = raw.data[i];
-    //         if (d < start) {
-    //             d = 0;
-    //         } else if (d > end) {
-    //             d = 255;
-    //         } else {
-    //             d = ((d - start) / (end - start)) * 255;
-    //         }
-    //         pixels.data[i] = d;
-    //     }
-
-    //     ctx.putImageData(pixels, 0, 0);
-    // };
-    
-
     var chartRange = function(values, color) {
         var circle = svg.selectAll("rect")
         .data(values)
         .attr("x", function(d, i) { return d * (testWidth/n); })
         .attr('fill', color);
     };
+};
 
 
-    model.on('change:theZ change:theT change:channels', function(model){
-        console.log("Z/T or channels changed");
+$(document).ready(function(){
+    
 
-        var cStrings = [];
-        _.each(model.get('channels'), function(c, i){
-            cStrings.push(1+i + "|" + c.window.start + ":" + c.window.end + "$" + c.color);
-        });
-        var renderString = cStrings.join(","),
-            imageId = model.get('id'),
-            theZ = model.get('theZ'),
-            theT = model.get('theT');
+    var model = new ViewerModel();
 
-        var imgSrc = '/webgateway/render_image/' + imageId + "/" + theZ + "/" + theT +
-                '/?c=' + renderString + "&m=c";
-
-        console.log('updateImage', imgSrc);
-        $("#viewer").attr('src', imgSrc);
-    });
-
-    $("#zSlider").on('input', function(){
-        $("#zIndex").html($(this).val());
-    }).on('change', function(){
-        var theZ = $(this).val();
-        model.set('theZ', theZ);
-    });
+    new ChannelSliders(model);
+    new Histogram(model);
+    new Zslider(model);
+    new ImageViewer(model);
 
     // start everything by loading image
     model.loadData(IMAGE_ID);
+
 });
