db55a66598817d51db0d11a7d284a988ee66d1f5
William Moore <w.moore@dundee.ac.uk>
2016-05-30 15:53:01 +0100

    flake8 fix

--- urls.py
+++ urls.py
@@ -90,6 +90,7 @@ urlpatterns = patterns(
     url(r'^histogram_viewer/(?:(?P<iid>[0-9]+)/)?$', views.histogram_viewer,
         name="webtest_histogram_viewer"),
 
-    url(r'^histogram_data/(?P<iid>[0-9]+)/channel/(?P<theC>[0-9]+)/', views.histogram_data,
+    url(r'^histogram_data/(?P<iid>[0-9]+)/channel/(?P<theC>[0-9]+)/',
+        views.histogram_data,
         name="webtest_histogram_data"),
 )
