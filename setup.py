eb93340841a270d2363bcaa6faa90a9724edc742
Sebastien Besson <seb.besson@gmail.com>
2019-11-22 11:06:06 +0000

    Add deploy to Travis CI and update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-VERSION = '0.3.1'
+VERSION = '0.4.dev1'
 
 
 setup(name="omero-webtest",
@@ -49,6 +49,7 @@ setup(name="omero-webtest",
           'Operating System :: OS Independent',
           'Programming Language :: JavaScript',
           'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
           'Topic :: Internet :: WWW/HTTP',
           'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
           'Topic :: Internet :: WWW/HTTP :: WSGI',
